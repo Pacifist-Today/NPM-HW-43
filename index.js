b588d9ce74d6f9501621a7372d5a9bcb805d7446
Pacifist <desmondmorales92@gmail.com>
2022-08-20 17:23:00 +0300

    testing npm, lodash, prettier, eslint, airbnb

---
+++ index.js
@@ -0,0 +1,17 @@
+var name = 'Cool';
+
+function calculation(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+calculation(3, 4);
+subtraction(3, 4);
+multiplication(3, 4);
